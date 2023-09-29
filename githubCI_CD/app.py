e4ee54274d8a031df39d449d8d4eeb6ffc458513
tatchum237 <tatchumdefo@gmail.com>
2023-09-29 09:02:24 +0100

    clean data

--- githubCI_CD/app.py
+++ githubCI_CD/app.py
@@ -4,9 +4,12 @@ import pandas as pd
 def load_data(url = 'data/house.csv'):
     return pd.read_csv(url)
 
+def clean_data(data):
 
-
-
+    # Gérer les valeurs manquantes en les remplaçant
+    for column in data.columns:
+        data[column].fillna(data[column].mean, inplace=True)
+    return data
 
 
 
