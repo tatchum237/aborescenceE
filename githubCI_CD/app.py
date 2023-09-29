7fbbb3ad34cf5901a6f05fcab9f56f7ad8ef107b
tatchum237 <tatchumdefo@gmail.com>
2023-09-29 09:52:50 +0100

    fin

--- githubCI_CD/app.py
+++ githubCI_CD/app.py
@@ -5,7 +5,8 @@ def load_data(url = 'data/house.csv'):
     return pd.read_csv(url)
 
 def clean_data(data):
-
+    # Supprimer les lignes dupliquées
+    data = data.drop_duplicates()
     # Gérer les valeurs manquantes en les remplaçant
     for column in data.columns:
         data[column].fillna(data[column].mean, inplace=True)
@@ -14,27 +15,19 @@ def clean_data(data):
 def delete_data(data):
     # Supprimer les lignes dupliquées
     data = data.drop_duplicates()
-    data = data.drop(
-        ['Unnamed: 0', 'rent_per_day', 'dimension', 'tv_liveroom', 'colocation', 'tv_all_bedroom', 'climatisation',
-         'wifi', 'gym', 'hot_water', 'swimming_pool', 'cooker', 'freezer', 'is_booked.1', 'is_booked', 'fence',
-         'elevator', 'pets_allowed', 'is_furnished', 'is_finished', 'events_allowed', 'suitable_for_children',
-         'smooking_allowed', 'is_negociable', 'name', 'id', 'disponible', 'gcel', 'desciption', 'publish', 'ia',
-         'water_m3_price', 'electricity_Kw_price', 'number_of_person_allowed', 'moyenne', 'nb_partageur',
-         'nb_reserveur', 'user', 'sexe_colocataire', 'age_colocataire', 'religion_colocataire',
-         'religion_colocataire.1', 'checkin_time', 'checkout_time', 'date_created', 'lien', 'partageurs', 'reserveurs'],
-        axis=1)
-    return data
 
+    return data
 
 def feature_enineer(data):
     data['city'] = data['city'].apply(lambda x: call_name_pays(x))
     data['country'] = data['country'].apply(lambda x: call_name(x))
-    data['date_created'] = data['date_created'].apply(lambda x: x.split('/')[0])
+
     return data
 
+
 def call_name(dat):
-  pays = dat[28:50]
-  return pays.split('\'')[0]
+    pays = dat[28:50]
+    return pays.split('\'')[0]
 
 
 def call_name_pays(dat):
@@ -43,20 +36,24 @@ def call_name_pays(dat):
 
 
 def show_stat(data):
-    data[['renting_price', 'is_immeuble']].groupby(['is_immeuble'], as_index=False).mean().sort_values(
-        by='renting_price', ascending=False)
-    data[['renting_price', 'is_moderne']].groupby(['is_moderne'], as_index=False).mean().sort_values(by='renting_price',
-                                                                                                     ascending=False)
+    pass
+   
+   
 
-    data[['city', 'renting_price', 'is_immeuble', 'category']].groupby(['city', 'is_immeuble', 'category'],
-                                                                       as_index=False).sum().sort_values(
-        by='renting_price', ascending=False)
+#if __name__=="__main__":
+# Charger vos données
+
+data = load_data()
+
+# Appeler les fonctions en série
+data = clean_data(data)
+
+data = delete_data(data)
+
+data = feature_enineer(data)
 
 
-
-
-
-
+show_stat(data)
 
 
 
